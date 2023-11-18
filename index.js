2b5b1f89e0f7f466fa6cda163b2a4ec0e85376d0
Ctbenoit60 <ctbenoit60@gmail.com>
2023-11-18 13:13:50 -0600

    created an array of artists then wrote a function that created a card for each artist with their art showing

--- index.js
+++ index.js
@@ -1,23 +1,74 @@
-function addCard(title = " Untitles", text = "Missing Text") {
-    // clone the template
-    const template = 
-    document.getElementById("card-template").content.cloneNode(true);
+function addCard(title = " Untitles", text = "Missing Text", imageURL) {
+  // clone the template
+  const template = document
+    .getElementById("card-template")
+    .content.cloneNode(true);
 
-    // populate the template
-    template. querySelector('.card-title').innerText = title;
-    template. querySelector('.card-text').innerText = text;
+  // populate the template
+  template.querySelector(".card-img-top").src = imageURL;
+  template.querySelector(".card-title").innerText = title;
+  template.querySelector(".card-text").innerText = text;
 
-    //include the populated template into the page
-    document.querySelector('#card-list').appendChild(template);
+  //include the populated template into the page
+  document.querySelector("#card-list").appendChild(template);
 }
 
 addCard();
 
 const data = [
-    { name: "Christina", age: 30 },
-    { name: "Sam", age: 31 },
+  { name: "Christina", age: 30 },
+  { name: "Sam", age: 31 },
+];
+
+data.forEach((person) => {
+  addCard(person.name, `Age: ${person.age}`);
+});
+
+const artists = [
+    {
+      name: "Van Gogh",
+      portfolio: [
+        {
+          title: "portrait",
+          url: "https://collectionapi.metmuseum.org/api/collection/v1/iiif/436532/1671316/main-image",
+        },
+        {
+          title: "sky",
+          url: "https://mymodernmet.com/wp/wp-content/uploads/2020/11/White-house-night-van-goh-worldwide-2.jpg",
+        },
+      ],
+    },
+    {
+      name: "Pablo Picasso",
+      portfolio: [
+        {
+          title: "cubism",
+          url: "https://www.pablopicasso.org/images/paintings/guernica.jpg",
+        },
+        {
+          title: "blue period",
+          url: "https://www.pablopicasso.org/images/paintings/old-guitarist.jpg",
+        },
+      ],
+    },
+    {
+      name: "Leonardo da Vinci",
+      portfolio: [
+        {
+          title: "Mona Lisa",
+          url: "https://www.leonardodavinci.net/images/gallery/mona-lisa.jpg",
+        },
+        {
+          title: "The Last Supper",
+          url: "https://www.leonardodavinci.net/images/gallery/the-last-supper.jpg",
+        },
+      ],
+    },
   ];
   
-  data.forEach(person => {
-      addCard(person.name, `Age: ${person.age}`)
-  })
+  artists.forEach((artist) => {
+    artist.portfolio.forEach((artwork) => {
+      addCard(artist.name, artwork.title, artwork.url);
+    });
+  });
+  
